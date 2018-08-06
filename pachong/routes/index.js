f322b81cdfbd5cc321bae43bc3375255e0e0f8fb
tuyao <450175465@qq.com>
2018-08-06 09:27:46 +0800

    a

--- pachong/routes/index.js
+++ pachong/routes/index.js
@@ -17,13 +17,14 @@ router.get('/', function(req, res, next) {
     function runAsync() {
         var data = new Promise(function (resolve, reject) {
 
-            http.get("http://sports.163.com/world", function (res) {
+            http.get("https://www.qichacha.com/search_index?key=%25E6%2595%2599%25E8%2582%25B2&ajaxflag=1&province=BJ&sortField=startdate-false&", function (res) {
 
                 var html = "";
 
                 res.on('data', function (chunk) {
 
                     html += iconv.decode(chunk, 'GBK')
+                    console.log(html)
 
                 });
 
@@ -48,11 +49,11 @@ router.get('/', function(req, res, next) {
 
     runAsync().then(function ($) {
 
-         var text=$(".breadcrumbs").text();
+         //var text=$(".breadcrumbs").text();
 
         console.log(text);
 
-        res.send(text);
+        res.send("成功");
 
     }).catch(function()
     {
---
+++ thinkjs3/demo/src/controller/index.js
@@ -0,0 +1,26 @@
+const Base = require('./base.js');
+
+const cheerio=require('cheerio');
+
+const fly=require("flyio")
+
+module.exports = class extends Base {
+  async  indexAction() {
+
+
+    function getFromApi()
+    {
+
+      return fly.get('https://www.qichacha.com/search_index?key=%25E6%2595%2599%25E8%2582%25B2&ajaxflag=1&province=BJ&sortField=startdate-false&');
+
+
+    }
+
+    const value = await getFromApi();
+
+
+    return this.success({name:"12388889888",data:value.data},"我们88888888");
+
+
+  }
+};
