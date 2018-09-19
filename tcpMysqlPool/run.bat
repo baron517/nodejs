822f6bb9ca637bc024e41b64e547a1618d341742
tuyao <450175465@qq.com>
2018-09-19 16:57:57 +0800

    a

---
+++ tcpMysqlPool/run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd /d %~dp0
+node server.js
+
+pause
--- thinkjs3/demo/src/controller/index.js
+++ thinkjs3/demo/src/controller/index.js
@@ -8,18 +8,33 @@ module.exports = class extends Base {
   async  indexAction() {
 
 
-    function getFromApi()
+   /* function getFromApi()
     {
 
-      return fly.get('https://www.qichacha.com/search_index?key=%25E6%2595%2599%25E8%2582%25B2&ajaxflag=1&province=BJ&sortField=startdate-false&');
+      return fly.get('https://tl.pro.youzewang.com/Api/CommonApi/test');
 
 
     }
 
-    const value = await getFromApi();
+    const value = await getFromApi();*/
 
 
-    return this.success({name:"12388889888",data:value.data},"我们88888888");
+    for(var i=0;i<5000;i++)
+    {
+      console.log(i);
+      fly.post('https://tl.pro.youzewang.com/Api/CommonApi/test', {
+            code:1000,msg:"success",data:"试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试"
+          })
+          .then(function (response) {
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            //console.log(error);
+          });
+
+    }
+
+    return this.success({name:"12388889888"},"我们88888888");
 
 
   }
